e6bac61bf9955cd7ea9eeac4f2dbe974cc1a5938
Candylifter <candylifter@gmail.com>
2016-07-21 17:18:30 +0300

    Add css pointer:crosshair, move functions to content.js

--- content.js
+++ content.js
@@ -1,3 +1,51 @@
-function alertVisitor(msg) {
-  alert('Yo, msg for ya: ' + msg);
+const HIGHLIGHT_CLASSNAME = 'dashboardify-highlight';
+
+function disableEvents(callback) {}
+
+function getPathTo(element) {
+    if (element.tagName == 'HTML')
+        return '/HTML[1]';
+    if (element===document.body)
+        return '/HTML[1]/BODY[1]';
+
+    var ix= 0;
+    var siblings= element.parentNode.childNodes;
+    for (var i= 0; i<siblings.length; i++) {
+        var sibling= siblings[i];
+        if (sibling===element)
+            return getPathTo(element.parentNode)+'/'+element.tagName+'['+(ix+1)+']';
+        if (sibling.nodeType===1 && sibling.tagName===element.tagName)
+            ix++;
+    }
+}
+
+function alertXPath() {
+  alert(getPathTo(this));
+  this.classList.remove(HIGHLIGHT_CLASSNAME);
+  this.removeEventListener('click', alertXPath);
+  document.body.onmouseover = undefined;
+}
+
+function highlightDOMElements() {
+  var prev;
+
+  document.body.onmouseover = handler;
+
+  function handler(event) {
+    if (event.target === document.body || (prev && prev === event.target)) {
+      return;
+    }
+    if (prev) {
+      prev.removeEventListener('click', alertXPath);
+
+      prev.classList.toggle(HIGHLIGHT_CLASSNAME);
+      prev = undefined;
+    }
+
+    if (event.target) {
+      prev = event.target;
+      prev.classList.toggle(HIGHLIGHT_CLASSNAME);
+      prev.addEventListener('click', alertXPath);
+    }
+  }
 }
